1cae66b1c5c3dc0df28a69b79d719cb7090d42b8
Ático Mismana <aticomismana7@gmail.com>
2017-03-02 15:18:35 -0300

    implementando tiposPrimitivos

---
+++ 01/tiposPrimitivos/tiposPrimitivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiposPrimitivos
+{
+    class Program
+    {
+        enum Dias { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado }
+        enum Dias1 { Domingo=11, Segunda=12, Terça=13, Quarta=14, Quinta=15, Sexta=16, Sábado=17 }
+
+        static void Main(string[] args)
+        {
+            int numero = 3;
+            Console.WriteLine(numero);
+
+            float numero2 = 2;
+            int resultado = numero + (int) numero2;
+            Console.WriteLine(resultado);
+
+            String text = "Olá mundo";
+            Console.WriteLine(text);
+
+            bool certidaoDeVivo = false;
+
+            Console.WriteLine("Você está vivo? {0}",certidaoDeVivo);
+
+            Dias dia = Dias.Domingo;
+            Console.WriteLine(dia);
+            Console.WriteLine("Dia referencia numerica: {0}", (int)dia);
+
+            Dias1 dia1 = Dias1.Segunda;
+            Console.WriteLine(dia1);
+            Console.WriteLine("Dia referencia numerica: {0}", (int)dia1);
+
+            Console.ReadLine();
+            // comentario
+        }
+    }
+}
